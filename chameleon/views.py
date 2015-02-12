e4a0b245e668c6e6fde4ba1556767d8e8b444458
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-02-12 10:10:21 -0600

    put dashboard back here

--- chameleon/views.py
+++ chameleon/views.py
@@ -1,8 +1,11 @@
-# from django.core.urlresolvers import reverse
-# from django.shortcuts import render
-# from django.views import generic
-# from django.contrib.auth.decorators import login_required
-# from github_content import util as content_util
-#
-# def newsletter( request ):
-#     return render( request, 'newsletter.html' )
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard( request ):
+    actions = []
+    actions.append( { 'name': 'Manage your Projects', 'url': reverse( 'projects:user_projects' ) } )
+    actions.append( { 'name': 'Help Desk Tickets', 'url': reverse( 'djangoRT:mytickets' ) } )
+    actions.append( { 'name': 'Manage Your Account', 'url': reverse( 'tas:profile' ) } )
+    return render( request, 'dashboard.html', { 'actions': actions })
