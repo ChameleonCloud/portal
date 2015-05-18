7df7362f49ffa6e19e3b1cc265458ef67d65cb93
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-05-18 16:58:21 -0500

    add confirmation screens for admin actions

--- tas/admin.py
+++ tas/admin.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.contrib import admin, messages
+from django.contrib.admin import helpers
 from django.contrib.admin.options import IS_POPUP_VAR
 from django.contrib.admin.utils import unquote
 from django.contrib.auth.admin import UserAdmin
@@ -15,60 +16,6 @@ from .forms import TasUserProfileAdminForm
 import logging
 
 logger = logging.getLogger('default');
-
-def user_edit(request):
-    data = request.POST.copy()
-    num_users = data['num_users']
-    tas = TASClient()
-
-    user = {}
-    usernameList = []
-    for x in range (0, int(num_users)):
-        user['username'] = data['username_'+str(x)]
-        user['firstName'] = data['firstName_'+str(x)]
-        user['lastName'] = data['lastName_'+str(x)]
-        user['email'] = data['email_'+str(x)]
-        user['source'] = data['source_'+str(x)]
-        user['citizenshipId'] = data['citizenshipId_'+str(x)]
-        user['citizenship'] = data['citizenship_'+str(x)]
-        user['countryId'] = data['countryId_'+str(x)]
-        user['country'] = data['country_'+str(x)]
-
-        deptId = int(data['department_'+str(x)])
-        instId = int(data['institution_'+str(x)])
-        dept = tas.get_department(instId, deptId)
-        inst = tas.get_institution(instId)
-
-        user['departmentId'] = int(deptId)
-        user['department'] = dept['name']
-        user['institutionId'] = int(instId)
-        user['institution'] = inst['name']
-
-        if 'piEligible_'+str(x) in data:
-            user['piEligibility'] = 'Eligible'
-        else:
-            user['piEligibility'] = 'Ineligible'
-
-        if 'resetPassword_'+str(x) in data:
-            try:
-                resp = tas.request_password_reset( user['username'], source='Chameleon' )
-                print resp
-            except:
-                print 'Failed password reset request'
-
-        user_result = tas.save_user(data['id_'+str(x)], user)
-
-
-        if user_result['username']:
-            usernameList.append(user_result['username'])
-            success = True
-
-    if success:
-        messages.success(request, 'TAS information successfully updated')
-    else:
-        messages.error(request, "There was an issue saving this user's TAS information. Please trying again later.")
-
-    return tas_users(request, usernameList)
 
 class ReadOnlyDirectToTasWidget(forms.Widget):
     def render(slef, name, value, attrs):
@@ -104,50 +51,72 @@ class TasUserAdmin(UserAdmin):
         (_('Account'), { 'fields': ('username', ) }),
     ) + UserAdmin.fieldsets[2:]
 
-    # def tas_user_info(self, request, username):
-    #
-    #     return render(request, 'admin/admin.html', context)
+    def reset_user_password(self, request, selected_users, form_url=''):
 
-    def reset_user_password(self, request, selected_users):
-        tas = TASClient()
-        success = False
+        # The user has already confirmed the action.
+        # Trigger password reset notifications
+        if request.POST.get('post'):
+            tas = TASClient()
+            for user in selected_users:
+                try:
+                    resp = tas.request_password_reset(user.username, source='Chameleon')
+                    self.message_user(request, _('Password Reset notification sent to: %s') % user.username)
+                except:
+                    logger.exception( 'Failed password reset request' )
+                    self.message_user(request, _('Unable to reset password for: %s') % user.username, level=messages.ERROR)
 
-        for username in selected_users:
-            try:
-                resp = tas.request_password_reset(username, source='Chameleon' )
+            return None
 
-                if resp:
-                    success = True
-            except:
-                logger.exception( 'Failed password reset request' )
+        context = {
+            'title': _('Send Password Reset Notification'),
+            'opts': self.model._meta,
+            'selected_users': selected_users,
+            'form_url': form_url,
+            'action_checkbox_name':helpers.ACTION_CHECKBOX_NAME
+        }
+        context.update(admin.site.each_context())
 
-        if success:
-            messages.success(request, 'Password reset email triggered')
-        else:
-            messages.error(request, 'There was an issue triggering the password reset email')
+        return TemplateResponse(request,
+            'admin/tas/reset_user_password.html',
+            context, current_app=self.admin_site.name)
 
-    def make_user_pi(self, request, selected_users):
-        tas = TASClient()
-        success = False
+    reset_user_password.short_description = "Send password reset"
 
-        for username in selected_users:
-            try:
-                tas_user = tas.get_user(username=username)
-                tas_user['piEligibility'] = 'Eligible'
-                user_result = tas.save_user(tas_user['id'], tas_user)
+    def make_user_pi(self, request, selected_users, form_url=''):
 
-                if user_result['username']:
-                    success = True
-            except:
-                logger.exception( 'Failed user save request' )
+        # The user has already confirmed the action.
+        # Make the users PI Eligible
+        if request.POST.get('post'):
+            tas = TASClient()
+            for user in selected_users:
+                try:
+                    tas_user = tas.get_user(username=user.username)
+                    if tas_user['piEligibility'] == 'Eligible':
+                        self.message_user(request, _('User %s already PI Eligible') % user.username, level=messages.WARNING)
+                    else:
+                        tas_user['piEligibility'] = 'Eligible'
+                        tas.save_user(tas_user['id'], tas_user)
+                        self.message_user(request, _('Granted PI Eligible to %s') % user.username)
+                except:
+                    logger.exception('Unable to Grant PI Eligible to %s' % user.username)
+                    self.message_user(request, _('Unable to Grant PI Eligible to %s') % user.username, level=messages.ERROR)
 
-        if success:
-            messages.success(request, 'User granted PI eligibility')
-        else:
-            messages.error(request, 'There was an issue saving the user')
+            return None
 
-    reset_user_password.short_description = "Trigger Password Reset"
-    make_user_pi.short_description = "Make PI"
+        context = {
+            'title': _('Grant PI eligibility'),
+            'opts': self.model._meta,
+            'selected_users': selected_users,
+            'form_url': form_url,
+            'action_checkbox_name':helpers.ACTION_CHECKBOX_NAME
+        }
+        context.update(admin.site.each_context())
+
+        return TemplateResponse(request,
+            'admin/tas/make_user_pi.html',
+            context, current_app=self.admin_site.name)
+
+    make_user_pi.short_description = "Grant PI Eligibility"
 
     def tas_manage(self, request, id, form_url=''):
         if not self.has_change_permission(request):
