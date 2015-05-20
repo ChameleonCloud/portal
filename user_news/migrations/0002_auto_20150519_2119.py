0cd77aaf548a21f748b3293a824b5ae7b7306c08
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-05-20 09:47:01 -0500

    make cms plugin configurable, add page-style plugin

---
+++ user_news/migrations/0002_auto_20150519_2119.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user_news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='usernewspluginmodel',
+            name='display_events',
+            field=models.BooleanField(default=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='usernewspluginmodel',
+            name='display_news',
+            field=models.BooleanField(default=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='usernewspluginmodel',
+            name='display_outages',
+            field=models.BooleanField(default=True),
+            preserve_default=True,
+        ),
+    ]
--- user_news/models.py
+++ user_news/models.py
@@ -58,12 +58,23 @@ class Event(News):
     event_type = models.TextField(choices=EVENT_TYPES)
     event_date = models.DateTimeField('event date')
 
+    def save(self):
+        if not self.slug:
+            self.slug = '%s-%s' % (self.event_date.strftime('%y-%m-%d'), slugify(self.title))
+        super(Event,self).save()
+
 """
 Implementation for System Outages
 """
 class Outage(News):
+
     start_date = models.DateTimeField('start of outage')
     end_date = models.DateTimeField('expected end of outage')
+
+    def save(self):
+        if not self.slug:
+            self.slug = '%s-%s' % (self.start_date.strftime('%y-%m-%d'), slugify(self.title))
+        super(Outage,self).save()
 
 class OutageUpdate(News):
     original_item = models.ForeignKey(Outage)
@@ -102,3 +113,6 @@ User News CMS Plugin Model
 """
 class UserNewsPluginModel(CMSPlugin):
     limit = models.IntegerField(default=5)
+    display_news = models.BooleanField(default=True)
+    display_events = models.BooleanField(default=True)
+    display_outages = models.BooleanField(default=True)
