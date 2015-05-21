5aa79ecb3dc4c961ad27e0285f940fec9e6e41fd
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-05-21 16:34:27 -0500

    style cleanup; convert email confirmation to form

--- tas/forms.py
+++ tas/forms.py
@@ -20,21 +20,32 @@ PI_ELIGIBILITY = (
 # for i in inst:
 #     INSTITUTIONS += ((i['id'], i['name']))
 
+class EmailConfirmationForm(forms.Form):
+    code = forms.CharField(
+            label='Enter Your Verification Code',
+            required=True,
+            error_messages={
+                'required': 'Please enter the verification code you received via email.'
+            })
+
+    username = forms.CharField(
+            label='Enter Your Chameleon Username',
+            required=True)
 
 
-def _password_policy( password ):
+def _password_policy(password):
     """
     Checks the password for meeting the minimum password policy requirements;
 
     Must be a minimum of 8 characters in length
     Must contain characters from at least three of the following: uppercase letters, lowercase letters, numbers, symbols
     """
-    if len( password ) < 8:
+    if len(password) < 8:
         return False
 
     char_classes = 0
     for cc in ['[a-z]', '[A-Z]', '[0-9]', '[^a-zA-Z0-9]']:
-        if re.search( cc, password ):
+        if re.search(cc, password):
             char_classes += 1
 
     if char_classes < 3:
@@ -42,30 +53,40 @@ def _password_policy( password ):
 
     return True
 
-class PasswordResetRequestForm( forms.Form ):
-    username = forms.CharField( label='Enter Your Chameleon Username', required=True )
+class PasswordResetRequestForm(forms.Form):
+    username = forms.CharField(label='Enter Your Chameleon Username', required=True)
 
-class PasswordResetConfirmForm( forms.Form ):
-    username = forms.CharField( label='Enter Your Chameleon Username', required=True )
-    code = forms.CharField( label='Reset Code', required=True  )
-    password = forms.CharField( widget=forms.PasswordInput, label='Password', required=True )
-    confirm_password = forms.CharField( widget=forms.PasswordInput, label='Confirm Password', required=True, help_text='Passwords must meet the following criteria: Must not contain your account name or parts of your full name; Must be a minimum of 8 characters in length; Must contain characters from at least three of the following: uppercase letters, lowercase letters, numbers, symbols' )
+class PasswordResetConfirmForm(forms.Form):
+    username = forms.CharField(label='Enter Your Chameleon Username', required=True)
+    code = forms.CharField(label='Reset Code', required=True)
+    password = forms.CharField(widget=forms.PasswordInput, label='Password', required=True)
+    confirm_password = forms.CharField(
+                                    widget=forms.PasswordInput,
+                                    label='Confirm Password',
+                                    required=True,
+                                    help_text=
+                                            'Passwords must meet the following criteria: '
+                                            'Must not contain your account name or parts of '
+                                            'your full name; Must be a minimum of 8 characters '
+                                            'in length; Must contain characters from at least '
+                                            'three of the following: uppercase letters, '
+                                            'lowercase letters, numbers, symbols')
 
-    def clean( self ):
+    def clean(self):
         cleaned_data = self.cleaned_data
-        password = cleaned_data.get( 'password' )
-        confirm_password = cleaned_data.get( 'confirm_password' )
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
 
         if password and confirm_password:
             if password != confirm_password:
-                self.add_error( 'password', 'The password provided does not match the confirmation' )
-                self.add_error( 'confirm_password', '' )
-                raise forms.ValidationError( 'The password provided does not match the confirmation' )
+                self.add_error('password', 'The password provided does not match the confirmation')
+                self.add_error('confirm_password', '')
+                raise forms.ValidationError('The password provided does not match the confirmation')
 
-            if not _password_policy( password ):
-                self.add_error( 'password', 'The password provided does not satisfy the password complexity requirements' )
-                self.add_error( 'confirm_password', '' )
-                raise forms.ValidationError( 'The password provided does not satisfy the password complexity requirements' )
+            if not _password_policy(password):
+                self.add_error('password', 'The password provided does not satisfy the password complexity requirements')
+                self.add_error('confirm_password', '')
+                raise forms.ValidationError('The password provided does not satisfy the password complexity requirements')
 
 
 class TasUserProfileForm(forms.Form):
@@ -82,7 +103,7 @@ class TasUserProfileForm(forms.Form):
         help_text="Faculty and Research Staff from U.S.-based institutions can "
         "request PI Eligibility. If you are not PI Eligible and would like to "
         "request to be eligible, please check the box above."
-        )
+       )
 
 class TasUserProfileAdminForm(TasUserProfileForm):
     """
@@ -95,32 +116,32 @@ class TasUserProfileAdminForm(TasUserProfileForm):
         label="Reset user's password",
         help_text="Check this box to reset the user's password. The user will be "
             "notified via email with instructions to complete the password reset."
-        )
+       )
 
-# class UserRegistrationForm( forms.Form ):
-#     firstName = forms.CharField( label='First Name', required=True )
-#     lastName = forms.CharField( label='Last Name', required=True )
-#     email = forms.EmailField( label='Email Address', required=True )
-#     institutionId = forms.CharField( label='Institution', required=True )
-#     countryId = forms.CharField( label='Country of Residence', required=True )
-#     citizenshipId = forms.CharField( label='Country of Citizenship', required=True )
+# class UserRegistrationForm(forms.Form):
+#     firstName = forms.CharField(label='First Name', required=True)
+#     lastName = forms.CharField(label='Last Name', required=True)
+#     email = forms.EmailField(label='Email Address', required=True)
+#     institutionId = forms.CharField(label='Institution', required=True)
+#     countryId = forms.CharField(label='Country of Residence', required=True)
+#     citizenshipId = forms.CharField(label='Country of Citizenship', required=True)
 #     piEligibility =
-#     username = forms.CharField( label='Username', required=True )
-#     password = forms.CharField( widget=forms.PasswordInput, label='Password', required=True )
-#     confirmPassword = forms.CharField( widget=forms.PasswordInput, label='Confirm Password', required=True )
+#     username = forms.CharField(label='Username', required=True)
+#     password = forms.CharField(widget=forms.PasswordInput, label='Password', required=True)
+#     confirmPassword = forms.CharField(widget=forms.PasswordInput, label='Confirm Password', required=True)
 #
-#     def clean( self ):
+#     def clean(self):
 #         cleaned_data = self.cleaned_data
-#         password = cleaned_data.get( 'password' )
-#         confirmPassword = cleaned_data.get( 'confirmPassword' )
+#         password = cleaned_data.get('password')
+#         confirmPassword = cleaned_data.get('confirmPassword')
 #
 #         if password and confirmPassword:
 #             if password != confirmPassword:
-#                 self.add_error( 'password', 'The password provided does not match the confirmation' )
-#                 self.add_error( 'confirmPassword', '' )
-#                 raise forms.ValidationError( 'The password provided does not match the confirmation' )
+#                 self.add_error('password', 'The password provided does not match the confirmation')
+#                 self.add_error('confirmPassword', '')
+#                 raise forms.ValidationError('The password provided does not match the confirmation')
 #
-#             if not _password_policy( password ):
-#                 self.add_error( 'password', 'The password provided does not satisfy the password complexity requirements' )
-#                 self.add_error( 'confirmPassword', '' )
-#                 raise forms.ValidationError( 'The password provided does not satisfy the password complexity requirements' )
+#             if not _password_policy(password):
+#                 self.add_error('password', 'The password provided does not satisfy the password complexity requirements')
+#                 self.add_error('confirmPassword', '')
+#                 raise forms.ValidationError('The password provided does not satisfy the password complexity requirements')
--- tas/views.py
+++ tas/views.py
@@ -7,13 +7,13 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django import forms
 from pytas.pytas import client as TASClient
-from tas.forms import PasswordResetRequestForm, PasswordResetConfirmForm
+from tas.forms import EmailConfirmationForm, PasswordResetRequestForm, PasswordResetConfirmForm
 import re
 import logging
 import json
 
 @login_required
-def profile( request ):
+def profile(request):
     context = {}
 
     try:
@@ -25,19 +25,19 @@ def profile( request ):
         # raise Exception('error loading profile')
 
     if context['profile']['source'] != 'Chameleon':
-        messages.info( request, 'Your account was created outside of the Chameleon Portal. Please visit the <a target="_blank" href="https://portal.tacc.utexas.edu">TACC User Portal</a> to edit your profile.' )
+        messages.info(request, 'Your account was created outside of the Chameleon Portal. Please visit the <a target="_blank" href="https://portal.tacc.utexas.edu">TACC User Portal</a> to edit your profile.')
 
     return render(request, 'tas/profile.html', context)
 
 @login_required
-def profile_edit( request ):
+def profile_edit(request):
     context = {}
 
     tas = TASClient()
     is_valid = True
 
     if request.method == 'POST':
-        data = tas.get_user( username=request.user )
+        data = tas.get_user(username=request.user)
 
         form_data = {
             'firstName': request.POST['firstName'],
@@ -57,52 +57,52 @@ def profile_edit( request ):
             data['piEligibility'] = 'Requested'
 
         try:
-            data[ 'institutionId' ] = int( form_data['institutionId'] )
+            data[ 'institutionId' ] = int(form_data['institutionId'])
         except:
             messages.error(request, 'Please select an institution.')
             is_valid = False
 
         try:
-            data[ 'departmentId' ] = int( form_data['departmentId'] )
+            data[ 'departmentId' ] = int(form_data['departmentId'])
         except:
             data[ 'departmentId' ] = None
 
         try:
-            data[ 'countryId' ] = int( form_data['countryId'] )
+            data[ 'countryId' ] = int(form_data['countryId'])
         except:
             messages.error(request, 'Please select your country of residence.')
             is_valid = False
 
         try:
-            data[ 'citizenshipId' ] = int( form_data['citizenshipId'] )
+            data[ 'citizenshipId' ] = int(form_data['citizenshipId'])
         except:
             messages.error(request, 'Please select your country of Citizenship.')
             is_valid = False
 
         if is_valid:
-            tas.save_user( data[ 'id' ], data )
-            messages.success( request, 'Your profile has been updated!' )
-            return HttpResponseRedirect( reverse( 'tas:profile' ) )
+            tas.save_user(data[ 'id' ], data)
+            messages.success(request, 'Your profile has been updated!')
+            return HttpResponseRedirect(reverse('tas:profile'))
         else:
             context['profile'] = data
             context['profile']['source'] = 'Chameleon'
 
     else:
         try:
-            resp = tas.get_user( username=request.user )
+            resp = tas.get_user(username=request.user)
             context['profile'] = resp
         except:
             context['profile'] = False
             # raise Exception('error loading profile')
 
     if context['profile']['source'] != 'Chameleon':
-        return HttpResponseRedirect( reverse( 'tas:profile' ) )
+        return HttpResponseRedirect(reverse('tas:profile'))
 
     try:
         inst = tas.institutions()
         context['institutions'] = inst
 
-        curr_inst = next( ( x for x in inst if x['id'] == context['profile']['institutionId'] ), None)
+        curr_inst = next((x for x in inst if x['id'] == context['profile']['institutionId']), None)
         context['curr_inst'] = curr_inst
     except Exception as e:
         logger = logging.getLogger('default')
@@ -119,23 +119,23 @@ def profile_edit( request ):
 
     return render(request, 'tas/profile_edit.html', context)
 
-def password_reset( request ):
+def password_reset(request):
     if request.user is not None and request.user.is_authenticated():
-        return HttpResponseRedirect( reverse( 'tas:profile' ) )
+        return HttpResponseRedirect(reverse('tas:profile'))
 
     if request.POST:
         if 'code' in request.GET:
-            form = PasswordResetConfirmForm( request.POST )
-            if _process_password_reset_confirm( request, form ):
-                messages.success( request, 'Your password has been reset! You can now log in using your new password' )
-                return HttpResponseRedirect( reverse( 'tas:profile' ) )
+            form = PasswordResetConfirmForm(request.POST)
+            if _process_password_reset_confirm(request, form):
+                messages.success(request, 'Your password has been reset! You can now log in using your new password')
+                return HttpResponseRedirect(reverse('tas:profile'))
         else:
-            form = PasswordResetRequestForm( request.POST )
-            if _process_password_reset_request( request, form ):
+            form = PasswordResetRequestForm(request.POST)
+            if _process_password_reset_request(request, form):
                 form = PasswordResetRequestForm()
 
     elif 'code' in request.GET:
-        form = PasswordResetConfirmForm( initial={ 'code': request.GET['code'] } )
+        form = PasswordResetConfirmForm(initial={ 'code': request.GET['code'] })
         form.fields['code'].widget = forms.HiddenInput()
     else:
         form = PasswordResetRequestForm()
@@ -147,85 +147,80 @@ def password_reset( request ):
 
     return render(request, 'tas/password_reset.html', { 'message': message, 'form': form })
 
-def _process_password_reset_request( request, form ):
+def _process_password_reset_request(request, form):
     if form.is_valid():
         # always show success to prevent data leaks
         messages.success(request, 'Your request has been received. If an account matching the username you provided is found, you will receive an email with further instructions to complete the password reset process.')
 
         username = form.cleaned_data['username']
         logger = logging.getLogger('auth')
-        logger.info( 'Password reset request for username: "%s"', username)
+        logger.info('Password reset request for username: "%s"', username)
         try:
             tas = TASClient()
-            user = tas.get_user( username=username )
-            resp = tas.request_password_reset( user['username'], source='Chameleon' )
-            logger.debug( resp )
+            user = tas.get_user(username=username)
+            resp = tas.request_password_reset(user['username'], source='Chameleon')
+            logger.debug(resp)
         except:
-            logger.exception( 'Failed password reset request' )
+            logger.exception('Failed password reset request')
 
         return True
     else:
         return False
 
-def _process_password_reset_confirm( request, form ):
+def _process_password_reset_confirm(request, form):
     if form.is_valid():
         data = form.cleaned_data
         try:
             tas = TASClient()
-            return tas.confirm_password_reset( data['username'], data['code'], data['password'], source='Chameleon' )
+            return tas.confirm_password_reset(data['username'], data['code'], data['password'], source='Chameleon')
         except Exception as e:
             logger = logging.getLogger('auth')
-            logger.exception( 'Password reset failed' )
-            if len( e.args ) > 1:
-                if re.search( 'account does not match', e.args[1] ):
-                    form.add_error( 'username', e.args[1] )
-                elif re.search( 'No password reset request matches', e.args[1] ):
-                    form.add_error( 'code', e.args[1] )
-                elif re.search( 'complexity requirements', e.args[1] ):
-                    form.add_error( 'password', e.args[1] )
+            logger.exception('Password reset failed')
+            if len(e.args) > 1:
+                if re.search('account does not match', e.args[1]):
+                    form.add_error('username', e.args[1])
+                elif re.search('No password reset request matches', e.args[1]):
+                    form.add_error('code', e.args[1])
+                elif re.search('complexity requirements', e.args[1]):
+                    form.add_error('password', e.args[1])
                 else:
-                    form.add_error( '__all__', 'An unexpected error occurred. Please try again' )
+                    form.add_error('__all__', 'An unexpected error occurred. Please try again')
             else:
-                form.add_error( '__all__', 'An unexpected error occurred. Please try again' )
+                form.add_error('__all__', 'An unexpected error occurred. Please try again')
 
     return False
 
-def email_confirmation( request ):
+def email_confirmation(request):
     context = {}
     if request.method == 'POST':
-        if request.POST['code'] and request.POST['username']:
-            context['code'] = request.POST['code']
-            context['username'] = request.POST['username']
+        form = EmailConfirmationForm(request.POST)
+        if form.is_valid():
+            code = request.POST['code']
+            username = request.POST['username']
             try:
                 tas = TASClient()
-                user = tas.get_user( username=context['username'] )
-                tas.verify_user( user['id'], context['code'] )
-                messages.success( request, 'Congratulations, your email has been verified! Please log in now.' )
-                return HttpResponseRedirect( reverse( 'tas:profile' ) )
+                user = tas.get_user(username=username)
+                tas.verify_user(user['id'], code)
+                messages.success(request, 'Congratulations, your email has been verified! Please log in now.')
+                return HttpResponseRedirect(reverse('tas:profile'))
             except Exception as e:
+                logger = logging.getLogger('auth')
+                logger.exception('Email verification failed')
                 if e[0] == 'User not found':
-                    messages.error( request, e[1] )
+                    form.add_error('username', e[1])
                 else:
-                    messages.error( request, 'Email verification failed. Please check your verification code and username and try again.' )
-        else:
-            if request.POST['code']:
-                context['code'] = request.POST['code']
-            else:
-                messages.error( request, 'Please enter the verification code you received via email' )
+                    form.add_error('code', 'Email verification failed. Please check your verification code and username and try again.')
 
-            if request.POST['username']:
-                context['username'] = request.POST['username']
-            else:
-                messages.error( request, 'Please verify your username' )
+    else:
+        form = EmailConfirmationForm(initial={'code': request.GET.get('code', '')})
 
-    elif 'code' in request.GET:
-        context['code'] = request.GET['code']
+    context['form'] = form
 
-    return render( request, 'tas/email_confirmation.html', context )
+    return render(request, 'tas/email_confirmation.html', context)
 
-def register( request ):
+def register(request):
     if request.user is not None and request.user.is_authenticated():
-        return HttpResponseRedirect( reverse( 'tas:profile' ) )
+        return HttpResponseRedirect(reverse('tas:profile'))
 
     tas = TASClient()
 
@@ -242,9 +237,9 @@ def register( request ):
             errors['lastName'] = 'Please provide your last name'
 
         if data['username']:
-            validate_username = validators.RegexValidator( '^[a-z][a-z0-9_]{2,7}$', 'Please enter a valid username.' )
+            validate_username = validators.RegexValidator('^[a-z][a-z0-9_]{2,7}$', 'Please enter a valid username.')
             try:
-                validate_username( data['username'] )
+                validate_username(data['username'])
             except ValidationError as e:
                 errors['username'] = e[0]
         else:
@@ -252,27 +247,27 @@ def register( request ):
 
         if data['email']:
             try:
-                validators.validate_email( data['email'] )
+                validators.validate_email(data['email'])
             except ValidationError as e:
                 errors['email'] = e[0]
         else:
             errors['email'] = 'Please provide a valid email address'
 
         if data['institutionId']:
-            data['institutionId'] = int( data['institutionId'] )
+            data['institutionId'] = int(data['institutionId'])
         else:
             errors['institutionId'] = 'Please select your affilitated Institution'
 
         if data['departmentId']:
-            data['departmentId'] = int( data['departmentId'] )
+            data['departmentId'] = int(data['departmentId'])
 
         if data['countryId']:
-            data['countryId'] = int( data['countryId'] )
+            data['countryId'] = int(data['countryId'])
         else:
             errors['countryId'] = 'Please provide your current country of residence'
 
         if data['citizenshipId']:
-            data['citizenshipId'] = int( data['citizenshipId'] )
+            data['citizenshipId'] = int(data['citizenshipId'])
         else:
             errors['citizenshipId'] = 'Please provide your country of citizenship'
 
@@ -287,7 +282,7 @@ def register( request ):
         else:
             errors['password'] = 'Please provide and confirm your password'
 
-        if len( errors ) == 0:
+        if len(errors) == 0:
             # success!
 
             # source
@@ -297,32 +292,32 @@ def register( request ):
             cleaned = data.copy()
             cleaned['password'] = cleaned['confirm_password'] = '********'
             logger = logging.getLogger('auth')
-            logger.info( 'processing user registration: data=%s', json.dumps( cleaned ) )
+            logger.info('processing user registration: data=%s', json.dumps(cleaned))
 
             try:
-                created_user = tas.save_user( None, data )
+                created_user = tas.save_user(None, data)
                 messages.success(request, 'Congratulations! Your account request has been received. Please check your email for account verification.')
-                return HttpResponseRedirect( '/' )
+                return HttpResponseRedirect('/')
             except Exception as e:
                 logger.exception('Error saving user')
                 if len(e.args) > 1:
-                    if re.search( 'DuplicateLoginException', e.args[1] ):
+                    if re.search('DuplicateLoginException', e.args[1]):
                         message = 'The username you chose has already been taken. Please choose another. If you already have an account with TACC, please log in using those credentials.'
-                        messages.error( request, message )
+                        messages.error(request, message)
                         errors['username'] = message
-                    elif re.search( 'DuplicateEmailException', e.args[1] ):
+                    elif re.search('DuplicateEmailException', e.args[1]):
                         message = 'This email is already registered. If you already have an account with TACC, please log in using those credentials.'
-                        messages.error( request, message + ' <a href="{0}">Did you forget your password?</a>'.format( reverse('tas:password_reset') ) )
+                        messages.error(request, message + ' <a href="{0}">Did you forget your password?</a>'.format(reverse('tas:password_reset')))
                         errors['email'] = message
-                    elif re.search( 'PasswordInvalidException', e.args[1] ):
+                    elif re.search('PasswordInvalidException', e.args[1]):
                         message = 'The password you provided did not meet the complexity requirements.'
-                        messages.error( request, message )
+                        messages.error(request, message)
                         errors['password'] = message
                         errors['confirm_password'] = message
                     else:
-                        messages.error( request, 'An unexpected error occurred. If this problem persists please create a help ticket.' )
+                        messages.error(request, 'An unexpected error occurred. If this problem persists please create a help ticket.')
                 else:
-                    messages.error( request, 'An unexpected error occurred. If this problem persists please create a help ticket.' )
+                    messages.error(request, 'An unexpected error occurred. If this problem persists please create a help ticket.')
         else:
             messages.error(request, 'There was an error in your registration. See below for details.')
 
@@ -344,7 +339,7 @@ def register( request ):
             context['curr_inst'] = None
     except Exception as e:
         logger = logging.getLogger('default')
-        logger.error( 'Error loading institutions', e )
+        logger.error('Error loading institutions', e)
         context['institutions'] = False
 
     try:
@@ -352,7 +347,7 @@ def register( request ):
         context['countries'] = countries
     except Exception as e:
         logger = logging.getLogger('default')
-        logger.error( 'Error loading countries', e )
+        logger.error('Error loading countries', e)
         context['countries'] = False
 
     return render(request, 'tas/register.html', context)
