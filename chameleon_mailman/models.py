c6a6ba725818c334c3288383ca2872b30dfa8f98
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-05-22 15:59:08 -0500

    add relationship name for subscriptions

--- chameleon_mailman/models.py
+++ chameleon_mailman/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 class MailmanSubscription(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='subscriptions')
     outage_notifications = models.BooleanField(
             default=True,
             help_text=_('Notifications about maintenance downtimes and outages'))
