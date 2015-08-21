3ff935d5235426367b99668d0a7bc6a7b5c8ad2e
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-08-21 13:16:41 -0500

    add pretty print option

--- util/templatetags/chameleon_tags.py
+++ util/templatetags/chameleon_tags.py
@@ -4,5 +4,5 @@ import json
 register = template.Library()
 
 @register.filter
-def to_json(value):
-    return json.dumps(value)
+def to_json(value, indent=None):
+    return json.dumps(value, indent=indent)
