23075af5e3ea6558bb70f658080b7ed130cacf71
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-09-11 10:01:55 -0500

    handle openid registration flow; only allow login for active users

--- chameleon_openid/views.py
+++ chameleon_openid/views.py
@@ -3,10 +3,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse
+from django.forms.util import ErrorList
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 import logging
+import re
 
 from openid.consumer import consumer
 from openid.consumer.discover import DiscoveryFailure
@@ -15,6 +17,11 @@ from openid.store.filestore import FileOpenIDStore
 
 from .models import OpenIDUserIdentity
 from .utils import JSONSafeSession, DBOpenIDStore
+
+from chameleon.decorators import anonymous_required
+from tas.forms import UserRegistrationForm, UserAccountForm
+from tas.views import _clean_registration_data
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +32,8 @@ def _get_openid_store():
 def _get_consumer(request):
     return consumer.Consumer(JSONSafeSession(request.session), _get_openid_store())
 
-# Create your views here.
+
+@anonymous_required
 def openid_login(request):
     if request.method == 'POST':
         openid_url = request.POST.get('openid_url')
@@ -55,6 +63,7 @@ def openid_login(request):
     return render(request, 'chameleon_openid/login.html')
 
 
+@anonymous_required
 def openid_callback(request):
 
     request_args = dict(request.GET.items())
@@ -88,9 +97,13 @@ def openid_callback(request):
 
             user = authenticate(openid_identity=result['url'])
             if user:
-                login(request, user)
-                messages.success(request, 'Login success using OpenID.')
-                return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, 'Login success using OpenID.')
+                    return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+                else:
+                    messages.error(request, 'Your account is not active yet. Please confirm your email address before logging in.')
+                    return HttpResponseRedirect('/')
             else:
                 return HttpResponseRedirect(reverse('chameleon_openid:openid_connect'))
 
@@ -105,6 +118,7 @@ def openid_callback(request):
     return HttpResponseRedirect(reverse('chameleon_openid:openid_login'))
 
 
+@anonymous_required
 def openid_connect(request):
     if 'openid' not in request.session:
         return HttpResponseRedirect(reverse('chameleon_openid:openid_login'))
@@ -149,6 +163,108 @@ def openid_connect(request):
         return render(request, 'chameleon_openid/connect_or_register.html', context)
 
 
+@anonymous_required
 def openid_register(request):
-    context = {}
+    openid = request.session['openid']
+    if request.method == 'POST':
+        profile_form = UserRegistrationForm(request.POST)
+        account_form = UserAccountForm(request.POST)
+        if profile_form.is_valid() and account_form.is_valid():
+            data = dict(profile_form.cleaned_data.items() + account_form.cleaned_data.items())
+
+            if request.POST.get('request_pi_eligibility'):
+                data['piEligibility'] = 'Requested'
+            else:
+                data['piEligibility'] = 'Ineligible'
+
+            data['source'] = 'Chameleon'
+            logger.info('Attempting OpenID user registration: %s' % _clean_registration_data(data))
+            try:
+                from pytas.pytas import client as TASClient
+                tas = TASClient()
+                created_user = tas.save_user(None, data)
+
+                # eager create local user and save OpenIDUserIdentity
+                local_user = get_user_model().objects.create_user(
+                        username=created_user['username'],
+                        email=created_user['email'],
+                        first_name=created_user['firstName'],
+                        last_name=created_user['lastName'],
+                        )
+
+                # set not active until email confirmation
+                local_user.is_active = False
+                local_user.save()
+                oid = OpenIDUserIdentity(uid = openid['url'], user = local_user)
+                oid.save()
+
+                messages.success(request,
+                                 'Congratulations! Your account request has been ' \
+                                 'received. Please check your email for account '\
+                                 'verification.'
+                                 )
+                return HttpResponseRedirect('/')
+            except Exception as e:
+                logger.exception('Error saving user')
+                if len(e.args) > 1:
+                    if re.search('DuplicateLoginException', e.args[1]):
+                        message = 'The username you chose has already been taken. ' \
+                                  'Please choose another. If you already have an ' \
+                                  'account with TACC, please log in using those ' \
+                                  'credentials.'
+                        errors = account_form._errors.setdefault('username', ErrorList())
+                        errors.append(message)
+                        messages.error(request, message)
+                    elif re.search('DuplicateEmailException', e.args[1]):
+                        forgot_password_url = reverse('tas:password_reset')
+                        message = 'This email is already registered. If you already ' \
+                                  'have an account with TACC, please log in using those ' \
+                                  'credentials. <a href="%s">Did you forget your password?</a>' \
+                                  % forgot_password_url
+
+                        messages.error(request, message)
+                        errors = profile_form._errors.setdefault('email', ErrorList())
+                        errors.append(message)
+                    elif re.search('PasswordInvalidException', e.args[1]):
+                        message = 'The password you provided did not meet the complexity requirements.'
+                        messages.error(request, message)
+                        errors = account_form._errors.setdefault('password', ErrorList())
+                        errors.append(message)
+                    else:
+                        messages.error(request,
+                                       'An unexpected error occurred. If this problem '\
+                                       'persists please create a help ticket.'
+                                       )
+                else:
+                    messages.error(request,
+                                   'An unexpected error occurred. If this problem ' \
+                                   'persists please create a help ticket.'
+                                   )
+    else:
+        full_name = openid['ax']['full_name'][0].split(' ')
+        profile_form = UserRegistrationForm(initial={
+                                                'email': openid['sreg']['email'],
+                                                'firstName': full_name[0],
+                                                'lastName': full_name[-1]
+                                            })
+        account_form = UserAccountForm(initial={'username': openid['sreg']['nickname']})
+        try:
+            username_taken = get_user_model().objects.get(username=openid['sreg']['nickname'])
+            if username_taken:
+                messages.warning(request,
+                                 'Your GENI nickname (<b>%s</b>) is not available on ' \
+                                 'Chameleon. Please choose a different username.' \
+                                 % openid['sreg']['nickname']
+                                 )
+        except:
+            pass
+
+    context = {
+        'profile_form': profile_form,
+        'account_form': account_form,
+        }
+
+    if request.method == 'POST':
+        context['request_pi_eligibility'] = request.POST.get('request_pi_eligibility')
+
     return render(request, 'chameleon_openid/register.html', context)
