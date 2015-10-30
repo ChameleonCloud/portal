3e0d598b64b342023c10aa9b90b71067c7276555
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-10-30 09:05:31 -0500

    update log settings

--- chameleon/settings.py
+++ chameleon/settings.py
@@ -201,18 +201,15 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(asctime)s %(message)s'
+        'default': {
+            'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s %(name)s.%(funcName)s: %(message)s'
         },
     },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+            'formatter': 'default',
         },
     },
     'loggers': {
