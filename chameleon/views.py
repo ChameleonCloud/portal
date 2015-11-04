767437669e6aec514de3b9fe32cc5fb3ae111b4e
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-11-04 10:33:20 -0600

    updated dashboard view with real info and geni federation prompts

--- chameleon/views.py
+++ chameleon/views.py
@@ -1,12 +1,68 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from user_news.models import Outage
+from djangoRT import rtUtil
+
+import logging
+from pytas.models import Project
+
+logger = logging.getLogger(__name__)
 
 @login_required
 def dashboard(request):
-    actions = [
-        {'name': 'Manage your Projects', 'url': reverse('projects:user_projects')},
-        {'name': 'Help Desk Tickets', 'url': reverse('djangoRT:mytickets')},
-        {'name': 'Manage Your Account', 'url': reverse('tas:profile')},
-    ]
-    return render(request, 'dashboard.html', {'actions': actions})
+    context = {}
+
+    # active projects...
+    projects = Project.list(username=request.user)
+    context['active_projects'] = [p for p in projects \
+                if p.source == 'Chameleon' and \
+                any(a.status in ['Active', 'Approved', 'Pending'] for a in p.allocations)]
+
+    # open tickets...
+    rt = rtUtil.DjangoRt()
+    context['open_tickets'] = rt.getUserTickets(request.user.email)
+
+    # ongoing outages...
+    outages = [o for o in Outage.objects.order_by('-end_date', '-start_date') if not o.resolved] # silly ORM quirk
+    context['outages'] = outages
+
+    # federation status...
+    if 'openid' in request.session:
+        on_geni_project, on_chameleon_project = _check_geni_federation_status(request)
+        context['geni_federation'] = {
+            'on_geni_project': on_geni_project,
+            'on_chameleon_project': on_chameleon_project,
+            'geni_project_name': settings.GENI_FEDERATION_PROJECTS['geni']['name'],
+            'chameleon_project_name': settings.GENI_FEDERATION_PROJECTS['chameleon']['name'],
+        }
+
+    return render(request, 'dashboard.html', context)
+
+def _check_geni_federation_status(request):
+    """
+    Checks if a user who authenticated view GENI/OpenID is on the Chameleon Federation
+    project on the GENI side. If so, then checks if the user is on the corresponding
+    project on the Chameleon side.
+
+    Returns a tuple of boolean values for (on_geni_project, on_chameleon_project)
+    """
+
+    geni_project_key = '%s|%s' % (settings.GENI_FEDERATION_PROJECTS['geni']['id'],
+                                  settings.GENI_FEDERATION_PROJECTS['geni']['name'])
+
+    on_geni_project = geni_project_key in request.session['openid']['ax']['projects']
+
+    if on_geni_project:
+        try:
+            fed_proj = Project(settings.GENI_FEDERATION_PROJECTS['chameleon']['id'])
+            on_chameleon_project = any(u.username == request.user for u in fed_proj.get_users())
+        except:
+            logger.warn('Could not locate Chameleon federation project: %s' % \
+                settings.GENI_FEDERATION_PROJECTS['chameleon'])
+            on_chameleon_project = False
+    else:
+        on_chameleon_project = False
+
+    return on_geni_project, on_chameleon_project
