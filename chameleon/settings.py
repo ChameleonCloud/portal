f9ed0da091d07769a51dbc9445f2d9213ba2a2bc
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-11-09 10:59:01 -0600

    add project urls to settings

--- chameleon/settings.py
+++ chameleon/settings.py
@@ -466,10 +466,12 @@ GENI_FEDERATION_PROJECTS = {
     'geni': {
         'id': '97762ef8-86ea-4212-b41a-ac5d75467c16',
         'name': 'Chameleon-Federation',
+        'url': 'https://portal.geni.net/secure/project.php?project_id=97762ef8-86ea-4212-b41a-ac5d75467c16',
     },
     'chameleon': {
         'id': '26427',
         'name': 'GENI-Federation',
+        'url': 'https://www.chameleoncloud.org/user/projects/26427/'
     },
 }
 
