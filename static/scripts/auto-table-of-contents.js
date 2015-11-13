c0be56fa22ff44d679a5726d1af91ab7089c3e1c
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-11-13 15:49:23 -0600

    fix responsive styles

--- static/scripts/auto-table-of-contents.js
+++ static/scripts/auto-table-of-contents.js
@@ -7,12 +7,17 @@
     var tocContent = $('.enable-toc');
     if (tocContent.length > 0) {
       $('body').addClass('with-toc');
-      $('<div id="toc">').appendTo('body').toc({
+
+      var firstHeader = $('h1,h2,h3,h4,h5,h6', tocContent).eq(0);
+      var el = $('<div id="toc">')
+      firstHeader.after(el);
+      el.toc({
         'container': tocContent
       });
+
       var toggle = $('<button name="toc-toggle" class="toc-toggle">');
       toggle.html('<span>Table of Contents</span>');
-      tocContent.prepend(toggle);
+      firstHeader.after(toggle);
       toggle.on('click', function() {
         $('body').toggleClass('with-toc');
       });
