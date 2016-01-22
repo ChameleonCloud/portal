eef9acce5f01ec42a321ab9c3f0463ed8e336aa6
Ajit Gauli <agauli@tacc.utexas.edu>
2016-01-22 16:11:32 -0600

    change image id date type to varchar

---
+++ appliance_catalog/migrations/0006_auto_20160122_2208.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('appliance_catalog', '0005_appliance_project_flagged'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='appliance',
+            name='chi_tacc_appliance_id',
+            field=models.CharField(max_length=100, unique=True, null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='appliance',
+            name='chi_uc_appliance_id',
+            field=models.CharField(max_length=100, unique=True, null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='appliance',
+            name='kvm_tacc_appliance_id',
+            field=models.CharField(max_length=100, unique=True, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
