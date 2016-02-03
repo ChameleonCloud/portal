5207cfc8cdb1071c948fda1de152f37c1a5815be
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-03 17:13:34 -0600

    django-angular reverse urls

--- chameleon/settings.py
+++ chameleon/settings.py
@@ -110,6 +110,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'djangular.middleware.DjangularUrlMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
