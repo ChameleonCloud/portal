36e84dc75797c157628fb3d373a0e2609cffb275
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-10 14:44:59 -0600

    add flag if docs present

--- appliance_catalog/serializers.py
+++ appliance_catalog/serializers.py
@@ -6,5 +6,5 @@ class MyJSONSerialiser(Serializer):
     def get_dump_object(self, obj):
         self._current['id'] = obj.pk
         if 'documentation' in self._current:
-            del self._current['documentation']
+            self._current['documentation'] = True if self._current['documentation'] is not None else False
         return self._current
