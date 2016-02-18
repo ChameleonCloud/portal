9f7cb2a59161c7675606cd75fcf048ef9bf003a2
Carrie Arnold <cmarnold@tacc.utexas.edu>
2016-02-18 14:16:19 -0600

    Adding tests

--- projects/tests.py
+++ projects/tests.py
@@ -31,6 +31,8 @@ class ProjectViewTests(TestCase):
         self.assertEqual(response['Location'],
             'http://testserver/login/?next=/user/projects/1234/')
 
+    # Test that a project with an active allocation up for renewal displays the recharge button
+    # User is PI
     @mock.patch('pytas.http.TASClient.get_project_users')
     @mock.patch('pytas.http.TASClient.project')
     def test_view_project(self, mock_tasclient_project, mock_tasclient_project_users):
@@ -54,6 +56,9 @@ class ProjectViewTests(TestCase):
             'data-allocation-id="1113" data-allocation-status="active"')
         self.assertContains(response, 'Recharge/extend allocation')
 
+    # Test that a project with an active allocation up for renewal does NOT display the recharge button
+    # if there is also a pending allocation
+    # User is PI
     @mock.patch('pytas.http.TASClient.get_project_users')
     @mock.patch('pytas.http.TASClient.project')
     def test_view_project_with_pending(self, mock_tasclient_project, mock_tasclient_project_users):
@@ -75,3 +80,52 @@ class ProjectViewTests(TestCase):
             'data-allocation-id="1115" data-allocation-status="pending"')
         self.assertNotContains(response, 'Recharge/extend allocation')
 
+    # Test that a project with a rejected allocation displays the resubmit button
+    # ONLY if there are no Pending or Active allocations
+    # User is PI
+    @mock.patch('pytas.http.TASClient.get_project_users')
+    @mock.patch('pytas.http.TASClient.project')
+    def test_view_allocation_rejected(self, mock_tasclient_project, mock_tasclient_project_users):
+        projects_fixture = json.loads(
+            open('projects/test_fixtures/project_1236.json').read())
+        mock_tasclient_project.return_value = projects_fixture
+
+        project_users_fixture = json.loads(
+            open('projects/test_fixtures/project_users.json').read())
+        mock_tasclient_project_users.return_value = project_users_fixture
+
+        self.client.login(username='jdoe1', password='password')
+        response = self.client.get(reverse('projects:view_project', args=[1236]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Chameleon Project Request Test')
+        self.assertContains(response,
+            'data-allocation-id="1115" data-allocation-status="inactive"')
+        self.assertContains(response,
+            'data-allocation-id="1114" data-allocation-status="rejected"')
+        self.assertContains(response, 'Resubmit allocation')
+
+    # Test that a project with a rejected allocation DOES NOT display the resubmit button
+    # if there are Pending or Active allocations
+    # User is PI
+    @mock.patch('pytas.http.TASClient.get_project_users')
+    @mock.patch('pytas.http.TASClient.project')
+    def test_view_allocation_rejected_with_active(self, mock_tasclient_project, mock_tasclient_project_users):
+        projects_fixture = json.loads(
+            open('projects/test_fixtures/project_1234.json').read())
+        mock_tasclient_project.return_value = projects_fixture
+
+        project_users_fixture = json.loads(
+            open('projects/test_fixtures/project_users.json').read())
+        mock_tasclient_project_users.return_value = project_users_fixture
+
+        self.client.login(username='jdoe1', password='password')
+        response = self.client.get(reverse('projects:view_project', args=[1235]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Chameleon Project Request Test')
+        self.assertContains(response,
+            'data-allocation-id="1114" data-allocation-status="active"')
+        self.assertContains(response,
+            'data-allocation-id="1115" data-allocation-status="pending"')
+        self.assertContains(response,
+            'data-allocation-id="1112" data-allocation-status="rejected"')
+        self.assertNotContains(response, 'Resubmit allocation')
