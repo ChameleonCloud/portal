c1d5ac11f788c99a8a7d0a60fc10876368f64f22
Carrie Arnold <cmarnold@tacc.utexas.edu>
2016-02-22 13:28:28 -0600

    Changed requirements for a dependency that had a breaking upgrade change

---
+++ usage/migrations/0002_auto_20160218_1443.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('usage', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='downtime',
+            name='nodes_down',
+            field=models.IntegerField(verbose_name=b'Down nodes'),
+            preserve_default=True,
+        ),
+    ]
