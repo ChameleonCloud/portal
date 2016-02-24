152f0b70077ff53c9c9ab0b8c927a9392664a091
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-24 13:37:20 -0600

    only add "help-block" if field has help_text

--- util/forms/renderers.py
+++ util/forms/renderers.py
@@ -5,5 +5,7 @@ from bootstrap3.text import text_value
 class ChameleonFieldRenderer(FieldRenderer):
 
     def append_to_field(self, html):
-        html = '<span class="help-block">{help}</span>'.format(help=self.field.help_text) + html
+        if self.field.help_text:
+            html = '<span class="help-block">{help}</span>'.format(
+                help=self.field.help_text) + html
         return html
