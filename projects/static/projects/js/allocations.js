89c12d008ebecd40835be474016fb7e6a2e4f516
Carrie Arnold <cmarnold@tacc.utexas.edu>
2016-07-14 15:51:53 -0500

    This branch is ready for testing

--- projects/static/projects/js/allocations.js
+++ projects/static/projects/js/allocations.js
@@ -9,14 +9,69 @@
             $('.allocation-rejected').toggleClass('hide');
     }
 
-    // initialize the chart
-    $('#usageChart').highcharts({
-                options: {
+       var start =  $('#usageChart').attr('start');
+       var end = $('#usageChart').attr('end');
+          $.ajax({
+            method: 'GET',
+            dataType: "json",
+            url: '/admin/usage/usage-by-users/' + $('#usageChart').attr('allocation-id') +'/?from=' + start + '&to=' + end,
+            cache: 'true'
+           }).done(function(response) {
+            // we're initializing all the queues ahead of time since we know what the are, and that will simplify the logic below
+            var data = {};
+            data['kvm@tacc'] = [];
+            data['kvm@uc'] = [];
+            data['chi@tacc'] = [];
+            data['chi@uc'] = [];
+
+            var seriesData = [];
+            var categories = [];
+            // response data:
+            // Object{ username = Object{ resource1 = total_usage, resource2 = total_usage }, username2 = Object{ resource2 = total_usage, resource3 = total_usage } }
+            for (var key in response) {
+              // push the username as a category
+              categories.push(key)
+
+              // for each category key, set the usage per resource for every username
+                if ('kvm@tacc' in response[key]) {
+                  data['kvm@tacc'].push(Math.ceil(response[key]['kvm@tacc']));
+                }
+
+                if ('kvm@uc' in response[key]) {
+                  data['kvm@uc'].push(Math.ceil(response[key]['kvm@uc']));
+                }
+
+                 if ('CHI@TACC' in response[key]) {
+                    data['chi@tacc'].push(Math.ceil(response[key]['CHI@TACC']));
+                }
+
+                 if ('CHI@UC' in response[key]) {
+                  data['chi@uc'].push(Math.ceil(response[key]['CHI@UC']));
+                }
+              }
+
+              // after the data has been aggregated, set the chart series
+              seriesData.push({
+                  name: 'kvm@tacc',
+                  data: data['kvm@tacc'],
+                }, {
+                  name: 'kvm@uc',
+                  data: data['kvm@uc'],
+                }, {
+                  name: 'chi@tacc',
+                  data: data['chi@tacc'],
+                }, {
+                  name: 'chi@uc',
+                  data: data['chi@uc'],
+              });
+
+                  // initialize the chart
+                $('#usageChart').highcharts({
                     chart: {
                         type: 'column'
                     },
                     xAxis: {
-                        categories: []
+                        categories: categories
                     },
                     yAxis: {
                         min: 0,
@@ -64,76 +119,13 @@
                     credits: {
                         enabled: false
                     },
-                },
                 title: {
                     text: ''
                 },
-                series: [],
+                series: seriesData,
                 useHighstocks: true,
-       });
+                });
 
-       var chart=Highcharts.charts[0];
-       var start =  $('#usageChart').attr('start');
-       var end = $('#usageChart').attr('end');
-          $.ajax({
-            method: 'GET',
-            dataType: "json",
-            url: '/admin/usage/usage-by-users/' + $('#usageChart').attr('allocation-id') +'/?from=' + start + '&to=' + end,
-            cache: 'true'
-           }).done(function(response) {
-            chart.series = [];
-            chart.options.xAxis.categories = [];
-            // we're initializing all the queues ahead of time since we know what the are, and that will simplify the logic below
-            var data = {};
-            data['kvm@tacc'] = [];
-            data['kvm@uc'] = [];
-            data['chi@tacc'] = [];
-            data['chi@uc'] = [];
-            // response data:
-            // Object{ username = Object{ resource1 = total_usage, resource2 = total_usage }, username2 = Object{ resource2 = total_usage, resource3 = total_usage } }
-            for (var key in response) {
-              // push the username as an xAxis key
-              chart.options.xAxis.categories.push(key)
-
-              // for each xAxis key, set the usage per resource for every username
-                if ('kvm@tacc' in response[key]) {
-                  data['kvm@tacc'].push(response[key]['kvm@tacc']);
-                } else {
-                  data['kvm@tacc'].push(0);
-                }
-
-                if ('kvm@uc' in response[key]) {
-                  data['kvm@uc'].push(response[key]['kvm@uc']);
-                } else {
-                  data['kvm@uc'].push(0);
-                }
-
-                 if ('CHI@TACC' in response[key]) {
-                    data['chi@tacc'].push(response[key]['CHI@TACC']);
-                } else {
-                  data['chi@tacc'].push(0);
-                }
-
-                 if ('CHI@UC' in response[key]) {
-                  data['chi@uc'].push(response[key]['CHI@UC']);
-                } else {
-                  data['chi@uc'].push(0);
-                }
-              }
-
-              // after the data has been aggregated, set the chart series
-              chart.series.push({
-                  name: 'kvm@tacc',
-                  data: data['kvm@tacc'],
-                }, {
-                  name: 'kvm@uc',
-                  data: data['kvm@uc'],
-                }, {
-                  name: 'chi@tacc',
-                  data: data['chi@tacc'],
-                }, {
-                  name: 'chi@uc',
-                  data: data['chi@uc'],
-              });
            });
+
 })( this, $ );
