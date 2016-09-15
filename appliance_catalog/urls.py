4379f281789596246580c71b7d17f23f30d59a07
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-15 08:43:15 -0500

    fix mixed spaces/tabs

--- appliance_catalog/urls.py
+++ appliance_catalog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from .views import ApplianceDeleteView
+
 urlpatterns = patterns(
     'appliance_catalog.views',
     url(r'^$', 'app_list', name='app_list'),
@@ -9,10 +10,9 @@ urlpatterns = patterns(
     url(r'^(?P<pk>\d+)/delete/$', ApplianceDeleteView.as_view(), name='app_delete'),
     url(r'^create/$', 'app_create', name='app_create'),
     url(r'^template/(?P<resource>.+?)\.html/$', 'app_template', name='app_template'),
-	url(r'^api/appliances/$', 'get_appliances', name='get_appliances'),
-	url(r'^api/appliances/(?P<pk>\d+)/$', 'get_appliance', name='get_appliance'),
+    url(r'^api/appliances/$', 'get_appliances', name='get_appliances'),
+    url(r'^api/appliances/(?P<pk>\d+)/$', 'get_appliance', name='get_appliance'),
     url(r'^api/appliances/(?P<pk>\d+)/template$', 'get_appliance_template', name='get_appliance_template'),
-	url(r'^api/keywords/$', 'get_keywords', name='get_keywords'),
-	url(r'^api/keywords/(?P<appliance_id>\d+)/$', 'get_keywords', name='get_keywords'),
-
+    url(r'^api/keywords/$', 'get_keywords', name='get_keywords'),
+    url(r'^api/keywords/(?P<appliance_id>\d+)/$', 'get_keywords', name='get_keywords'),
 )
