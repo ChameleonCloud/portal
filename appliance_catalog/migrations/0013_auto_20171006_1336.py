5b1b40f794bccc98a4652e0adaab668e8792005a
Alex Rocha <rochaa@tacc.utexas.edu>
2017-10-06 13:38:55 -0500

    adding upgrade migrations

---
+++ appliance_catalog/migrations/0013_auto_20171006_1336.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('appliance_catalog', '0012_appliance_needs_review'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='appliance',
+            name='keywords',
+            field=models.ManyToManyField(to='appliance_catalog.Keyword', through='appliance_catalog.ApplianceTagging', blank=True),
+        ),
+    ]
