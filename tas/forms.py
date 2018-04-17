4cc05b68a416dca325101a002f3932e7848f756c
Alex Rocha <rochaa@tacc.utexas.edu>
2018-04-17 14:23:10 -0500

    disabling edit citizenship

--- tas/forms.py
+++ tas/forms.py
@@ -167,6 +167,7 @@ class UserProfileForm(forms.Form):
 
         self.fields['countryId'].choices = get_country_choices()
         self.fields['citizenshipId'].choices = get_country_choices()
+        self.fields['citizenshipId'].widget.attrs['readonly'] = True
 
 
 class TasUserProfileAdminForm(forms.Form):
