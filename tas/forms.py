9b5914c5d6935d78fbe1bc6fa19adafb3e6df247
Alex Rocha <rochaa@tacc.utexas.edu>
2018-04-17 16:06:51 -0500

    Hiding citizenship in edit view

--- tas/forms.py
+++ tas/forms.py
@@ -154,8 +154,8 @@ class UserProfileForm(forms.Form):
     institutionId = forms.ChoiceField(label='Institution', choices=(), error_messages={'invalid': 'Please select your affiliated institution'})
     departmentId = forms.ChoiceField(label='Department', choices=(), required=False)
     title = forms.ChoiceField(label='Position/Title', choices=USER_PROFILE_TITLES)
-    countryId = forms.ChoiceField(label='Country of residence', choices=(), error_messages={'invalid': 'Please select your Country of residence'})
-    citizenshipId = forms.ChoiceField(label='Country of citizenship', choices=(), error_messages={'invalid': 'Please select your Country of citizenship'})
+    countryId = forms.CharField(label='Country of residence', widget=forms.HiddenInput(), error_messages={'invalid': 'Please select your Country of residence'})
+
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
@@ -166,8 +166,8 @@ class UserProfileForm(forms.Form):
             self.fields['departmentId'].choices = get_department_choices(data['institutionId'])
 
         self.fields['countryId'].choices = get_country_choices()
-        self.fields['citizenshipId'].choices = get_country_choices()
-        self.fields['citizenshipId'].widget.attrs['readonly'] = True
+        #self.fields['citizenshipId'].choices = get_country_choices()
+        self.fields['citizenshipId'].widget.attrs['disabled'] = True
 
 
 class TasUserProfileAdminForm(forms.Form):
