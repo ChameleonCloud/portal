8f2f085f0935ae6152f2874b65d03faa9886cc75
Alex Rocha <rochaa@tacc.utexas.edu>
2018-12-13 11:47:40 -0600

    sso callback template and redirect

--- chameleon/urls.py
+++ chameleon/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.views.generic import RedirectView
 from user_news.views import OutageListView, OutageDetailView, OutageFeed
+import views
 
 urlpatterns = patterns(
         '',
@@ -15,8 +16,6 @@ urlpatterns = patterns(
         url(r'^admin/allocations/', include('allocations.urls',
                                             namespace='allocations_admin')),
         url(r'^admin/usage/', include('usage.urls', namespace='usage_admin')),
-        # url(r'^appliance-catalog/', include('appliance_catalog.urls',
-        #                                     namespace='appliance_catalog')),
 
         # contrib urls
         url(r'^openid/', include('chameleon_openid.urls', namespace='chameleon_openid')),
@@ -29,6 +28,7 @@ urlpatterns = patterns(
         # custom urls
         #url(r'^login/', 'django.contrib.auth.views.login', name='login'),
         url(r'^login/', 'chameleon.os_login.custom_login', name='login'),
+        url(r'^sso/horizon/', 'chameleon.views.horizon_sso_login', name='horizon_sso_login'),
         url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'},
             name='logout'),
         url(r'^register/', RedirectView.as_view(url=reverse_lazy('tas:register'))),
@@ -59,6 +59,7 @@ urlpatterns = patterns(
         url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
         url(r'^', include('blog_comments.urls')),
         url(r'^', include('cms.urls')),
+
 )
 
 if settings.DEBUG:
