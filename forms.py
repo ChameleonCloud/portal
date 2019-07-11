53b8dbf97409f02000b7844e58f8c37634795a0b
Maxine King <maxineking@cs.uchicago.edu>
2019-07-11 17:42:56 -0500

    Working commit: and vs or option

--- forms.py
+++ forms.py
@@ -2,6 +2,6 @@ from django import forms
 from .models import Artifact, Author, Label
 
 class LabelForm(forms.Form):
-    label_options = [(label.id,label.label) for label in Label.objects.all()]
+    label_options = [(str(label.id),label.label) for label in Label.objects.all()]
     labels = forms.MultipleChoiceField(label='Labels',required=False, choices=label_options)
-        
+    is_or = forms.BooleanField(required=False)
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.template import loader
 
@@ -5,6 +6,22 @@ from django.views import generic
 from .models import Artifact, Author, Label
 
 from .forms import LabelForm
+
+def artifacts_from_form(data):
+    chosen_labels = data['labels']
+    if chosen_labels == []:
+        filtered = Artifact.objects.all() 
+    elif data['is_or']:
+        filtered = Artifact.objects.filter(labels__in=chosen_labels)
+    else:
+        
+            
+        q = Q()
+        for label in chosen_labels:
+            q &= Q(labels__in=label)
+        filtered = Artifact.objects.get(q)
+#        filtered = Artifact.objects.filter(labels__in=chosen_labels)
+    return filtered
 
 def index(request):
     template = loader.get_template('sharing/index.html')
@@ -16,9 +33,8 @@ def index(request):
             chosen_labels = form.cleaned_data['labels'] 
             context['form'] = form
             context['submitted'] = chosen_labels
-            filtered_set = Artifact.objects.filter(labels__in=chosen_labels)
-
-            context['artifacts'] = filtered_set
+#            context['artifacts'] = Artifact.objects.filter(labels=chosen_labels)
+            context['artifacts'] = artifacts_from_form(form.cleaned_data)
             return HttpResponse(template.render(context,request))
     else:
         form=LabelForm()
