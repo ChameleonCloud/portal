839b5112736798a845c4ce603cc504109941d6f2
Alex Rocha <xandro.rocha@gmail.com>
2020-05-27 12:48:19 -0500

    Logging claims info in debug log

--- chameleon/ChameleonOIDCAuthBackend.py
+++ chameleon/ChameleonOIDCAuthBackend.py
@@ -5,7 +5,7 @@ logger = logging.getLogger('auth')
 
 class ChameleonOIDCAB(OIDCAuthenticationBackend):
     def create_user(self, claims):
-        logger.info('Creating user from keycloak with claims: {0}'.format(claims))
+        logger.debug('Creating user from keycloak with claims: {0}'.format(claims))
         if self.get_settings('OIDC_USERNAME_ALGO', None):
             return super(ChameleonOIDCAB, self).create_user(claims)
         email = claims.get('email')
@@ -17,7 +17,7 @@ class ChameleonOIDCAB(OIDCAuthenticationBackend):
         return user
 
     def update_user(self, user, claims):
-        logger.info('Updating user from keycloak with claims: {0}'.format(claims))
+        logger.debug('Updating user from keycloak with claims: {0}'.format(claims))
         user.username = claims.get('preferred_username', '')
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
