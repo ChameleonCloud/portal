397edd6ea12e99cc9047ecd9c5101f5c4aa0d1c8
Alex Rocha <xandro.rocha@gmail.com>
2020-05-29 10:40:31 -0500

    cleaning up pi status assignment

--- chameleon/ChameleonOIDCAuthBackend.py
+++ chameleon/ChameleonOIDCAuthBackend.py
@@ -6,9 +6,9 @@ logger = logging.getLogger('auth')
 
 class ChameleonOIDCAB(OIDCAuthenticationBackend):
     def create_user(self, claims):
-        logger.debug('Creating user from keycloak with claims: {0}'.format(claims))
         if self.get_settings('OIDC_USERNAME_ALGO', None):
             return super(ChameleonOIDCAB, self).create_user(claims)
+        logger.debug('Creating user from keycloak with claims: {0}'.format(claims))
         email = claims.get('email')
         username = claims.get('preferred_username', '')
         user = self.UserModel.objects.create_user(username, email)
@@ -16,25 +16,22 @@ class ChameleonOIDCAB(OIDCAuthenticationBackend):
         user.last_name = claims.get('family_name', '')
         user.save()
         props = UserProperties(user=user)
-        if('principal_investigator' in claims.get('realm_access.roles', [])):
-            logger.debug('found principal_investigator status ' + \
-                str('principal_investigator' in claims.get('realm_access.roles', [])) + ' for user: ' + user.username)
-            props.is_pi_eligible = True
+        props.is_pi_eligible = ('principal_investigator' in claims.get('realm_access.roles', []))
         props.save()
         return user
 
     def update_user(self, user, claims):
-        try:
-            props = UserProperties.objects.get(user=user)
-        except UserProperties.DoesNotExist:
-            props = UserProperties(user=user)
-            logger.debug('found principal_investigator status ' + \
-                str('principal_investigator' in claims.get('realm_access.roles', [])) + ' for user: ' + user.username)
-            props.is_pi_eligible = ('principal_investigator' in claims.get('realm_access.roles', []))
-        props.save()
         logger.debug('Updating user from keycloak with claims: {0}'.format(claims))
         user.username = claims.get('preferred_username', '')
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
         user.save()
+
+        try:
+            props = UserProperties.objects.get(user=user)
+        except UserProperties.DoesNotExist:
+            props = UserProperties(user=user)
+        props.is_pi_eligible = ('principal_investigator' in claims.get('realm_access.roles', []))
+        props.save()
+
         return user
