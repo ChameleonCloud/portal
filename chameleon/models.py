e2839970569e0ad34ef17ac49de304ee2de6971a
Alex Rocha <xandro.rocha@gmail.com>
2020-06-16 14:53:06 -0500

    Switching to storing pi eligibility and audit log in portal

--- chameleon/models.py
+++ chameleon/models.py
@@ -3,4 +3,15 @@ from django.conf import settings
 
 class UserProperties(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
-    is_pi = models.BooleanField(default=False)
+    PI_STATUS_CHOICES = (
+        ('INELIGIBLE', 'Ineligible'),
+        ('ELIGIBLE', 'Eligible'),
+    )
+
+    pi_eligibility = models.CharField(max_length=50, blank=False,\
+        default='INELIGIBLE', choices=PI_STATUS_CHOICES)
+    pi_requested_at = models.DateTimeField('Date Requested', default=None)
+    pi_reviewed_at = models.DateTimeField('Date Reviewed', default=None)
+    pi_reviewer = models.ForeignKey(settings.AUTH_USER_MODEL,\
+        default=None,on_delete=models.CASCADE, related_name='+',)
+    pi_decision_summary = models.TextField(default=None)
