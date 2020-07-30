3b6047c1b7f3c871dde07ddbed2e047745e9eea4
Jason Anderson <jasonanderson@uchicago.edu>
2020-07-30 16:02:31 -0500

    Add search filter for list page

---
+++ sharing_portal/static/sharing_portal/index.js
@@ -0,0 +1,38 @@
+(function() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const filterInput = document.getElementById('cardFilter');
+
+    if (filterInput) {
+      filterInput.addEventListener('keyup', onFilterChange);
+    }
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const filter = queryParams.get('filter')
+    if (filter) {
+      applyQueryFilter(filter);
+      if (filterInput) {
+        filterInput.value = filter;
+      }
+    }
+  });
+
+  function onFilterChange(e) {
+    applyQueryFilter(e.target.value);
+  }
+
+  function applyQueryFilter(filter) {
+    const terms = filter.split(' ').filter(Boolean);
+    document.querySelectorAll('.cardItem').forEach((el) => {
+      const matches = terms.some((term) => {
+        return (term.length > 2) && el.dataset.search.includes(term);
+      });
+      el.style.display = matches ? '' : 'none';
+    });
+
+    if (history.replaceState) {
+      const queryParams = new URLSearchParams(window.location.search);
+      queryParams.set('filter', filter);
+      history.replaceState(null, '', '?' + queryParams.toString());
+    }
+  }
+})();
