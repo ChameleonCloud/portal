aef6a40478db6ace34ff7452064086db0eb67271
Jason Anderson <jasonanderson@uchicago.edu>
2020-08-20 13:18:38 -0500

    Remove legacy views

--- sharing_portal/urls.py
+++ sharing_portal/urls.py
@@ -12,12 +12,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/version/(?P<version_idx>\d+)$', views.artifact, name='detail_version'),
     url(r'^(?P<pk>\d+)/version/(?P<version_idx>\d+)/launch$', views.launch, name='launch_version'),
     url(r'^(?P<pk>\d+)/edit$', views.edit_artifact, name='edit'),
-    url(r'^edit$', views.edit_redirect, name='edit_redirect'),
-
-    # Deprecated
-    url(r'^upload$', views.upload, name='upload'),
-    url(r'^(?P<pk>\d+)/edit/sync_versions$', views.sync_artifact_versions, name='sync_versions'),
-    url(r'^edit/sync_versions$', views.sync_artifact_versions_redirect, name='sync_versions_redirect'),
 
     url(r'^create/embed$', views.embed_create, name='embed_create'),
     url(r'^(?P<pk>\d+)/edit/embed$', views.embed_edit, name='embed_edit'),
