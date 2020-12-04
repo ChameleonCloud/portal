2b8ee1967cac327a43a5d12652aefe1254dbc2a8
Jason Anderson <jasonanderson@uchicago.edu>
2020-12-04 12:20:40 -0600

    Enable managing artifact labels
    This updates the edit UI to allow updating artifact labels. These labels
    can be updated either within the embedded Jupyter edit form or on Trovi.
    This functionality already existed, but was never surfaced to the user.

--- sharing_portal/forms.py
+++ sharing_portal/forms.py
@@ -5,29 +5,10 @@ from projects.models import Project
 from .models import Artifact, ArtifactVersion, Author, Label
 
 
-class LabelForm(forms.Form):
-    """
-    Form to allow search on the index page
-    """
-
-    class LabelChoiceField(forms.ModelChoiceField):
-        """
-        Custom model choice field that can display a choice
-        of existing Label models.
-        """
-        def label_from_instance(self, obj):
-            return str(obj.id)
-
-    search = forms.CharField(required=False)
-    labels = LabelChoiceField(label='Labels', required=False,
-                              queryset=Label.objects.all())
-    is_or = forms.BooleanField(required=False)
-
-
 class ArtifactForm(forms.ModelForm):
     class Meta:
         model = Artifact
-        fields = ('title', 'short_description', 'description',)
+        fields = ('title', 'short_description', 'description', 'labels',)
 
 
 class ArtifactVersionForm(forms.ModelForm):
