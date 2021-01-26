0d9b3b879f8cf301eed9d9331167123e8c10796f
Mike <shermanm@uchicago.edu>
2021-01-26 14:52:40 -0500

    Feature/django rest framework (#151)
    Refactor API logic using django rest_framework and dynamic_rest
    
    add separate logic for:
    * querysets per model
    * serializers per model
    * filters per model
    * views per model
    * Add api url endpoints

--- allocations/urls.py
+++ allocations/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^view/$', views.view, name='view'),
-    url(r'^json/$', views.return_json, name='return_json'),
-    url(r'^approval/$', views.approval, name='approval'),
-    url(r'^denied/$', views.denied, name='denied'),
-    url(r'^user/$', views.user_select, name='user_select'),
-    url(r'^user/(?P<username>.+?)/$', views.user_projects, name='user_projects'),
-    url(r'^template/(?P<resource>.+?)\.html/$', views.allocations_template, name='allocations_template'),
+    url(r"^$", views.index, name="index"),
+    url(r"^view/$", views.view, name="view"),
+    url(r"^json/$", views.return_json, name="return_json"),
+    url(r"^approval/$", views.approval, name="approval"),
+    url(r"^denied/$", views.denied, name="denied"),
+    url(r"^user/$", views.user_select, name="user_select"),
+    url(r"^user/(?P<username>.+?)/$", views.user_projects, name="user_projects"),
+    url(
+        r"^template/(?P<resource>.+?)\.html/$",
+        views.allocations_template,
+        name="allocations_template",
+    ),
 ]
---
+++ util/querysets.py
@@ -0,0 +1,43 @@
+from projects.models import Project
+from allocations.models import Allocation
+from django.db.models import Max, Prefetch
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CCQuerySets:
+    def allocationsQuerySet(self):
+        """Get all allocations from django DB."""
+        logger.debug("Allocations QS INIT")
+        alloc_qs = Allocation.objects.select_related("requestor", "reviewer")
+        return alloc_qs
+
+    def orderedAllocations(self):
+        """Sort allocations by date requested."""
+        output_qs = self.allocationsQuerySet().order_by("date_requested").reverse()
+        return output_qs
+
+    def prefetchAllocations(self, filtered_qs, input_qs):
+        """Prefetch allocations, for use by other querysets"""
+        pf = Prefetch("allocations", queryset=filtered_qs)
+        output_qs = input_qs.prefetch_related(pf)
+        return output_qs
+
+    def projectsQuerySet(self):
+        """Get all projects from django DB."""
+        logger.debug("Projects QS INIT")
+        output_qs = Project.objects.select_related("type", "pi", "field")
+        return output_qs
+
+    def orderedProjects(self):
+        """Get projects, sorted by latest allocation."""
+        filtered_qs = self.orderedAllocations()
+        project_qs = self.projectsQuerySet()
+        output_qs = (
+            self.prefetchAllocations(filtered_qs, project_qs)
+            .annotate(latest_request=Max("allocations__date_requested"))
+            .order_by("-latest_request")
+        )
+        return output_qs
