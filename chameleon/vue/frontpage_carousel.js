3d1213815c8a3f88656c6a3b4b280c9353fed2f7
Jason Anderson <jasonanderson@uchicago.edu>
2021-03-08 17:21:10 -0600

    Styling tweaks to carousel
    This also removes some code we don't really need in the template directory
    (I think. It does not appear to be used magically to fill out any of the other
    DjangoCMS blog pages.)

--- chameleon/vue/frontpage_carousel.js
+++ chameleon/vue/frontpage_carousel.js
@@ -1,12 +1,14 @@
 import Glide from '@glidejs/glide'
-require('@glidejs/glide/dist/css/glide.core.min.css')
-require('@glidejs/glide/dist/css/glide.theme.min.css')
-require('./frontpage_carousel.css')
 
-new Glide('.glide', {
+/* The following imports are just to auto-load CSS onto the page */
+import GlideCoreCSS from '@glidejs/glide/dist/css/glide.core.min.css'
+import GlideThemeCSS from '@glidejs/glide/dist/css/glide.theme.min.css'
+import CarouselCSS from './frontpage_carousel.css'
+
+const glide = new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 1,
     autoplay: 5000,
     hoverpause: true
-}).mount()
+}).mount();
