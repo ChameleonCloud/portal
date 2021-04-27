120bbd5ab0d484dea61c51877b92f9fa34a1477c
Michael Sherman <shermanm@uchicago.edu>
2021-04-27 18:11:26 -0400

    add admin page for project invitations

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from projects.models import Publication
 
+from projects.models import Invitation, Publication
 
 admin.site.register(Publication)
+admin.site.register(Invitation)
