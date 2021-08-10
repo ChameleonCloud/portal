4dd08d2387b78788fe3111d298d7fa74c0acc15d
Mark Powers <markpowers@uchicago.edu>
2021-08-10 13:08:41 -0500

    Update code deprecated in 3.1

--- chameleon/urls.py
+++ chameleon/urls.py
@@ -3,7 +3,7 @@ import logging
 from chameleon_mailman import views as chameleon_mailman_views
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import REDIRECT_FIELD_NAME
@@ -50,96 +50,96 @@ class AdminOIDCLogin(View):
 urlpatterns = (
     [
         # admin urls
-        url(r"^admin/login/", AdminOIDCLogin.as_view()),
-        url(r"^admin/", admin.site.urls),
-        url(r"^admin/impersonate/", include("impersonate.urls")),
-        url(
+        re_path(r"^admin/login/", AdminOIDCLogin.as_view()),
+        re_path(r"^admin/", admin.site.urls),
+        re_path(r"^admin/impersonate/", include("impersonate.urls")),
+        re_path(
             r"^admin/allocations/",
             include("allocations.urls", namespace="allocations_admin"),
         ),
         # contrib urls
-        url(r"^oidc/", include("mozilla_django_oidc.urls")),
-        url(r"^ckeditor/", include("ckeditor_uploader.urls")),
-        url(r"^terms/", include("termsandconditions.urls")),
-        url(
+        re_path(r"^oidc/", include("mozilla_django_oidc.urls")),
+        re_path(r"^ckeditor/", include("ckeditor_uploader.urls")),
+        re_path(r"^terms/", include("termsandconditions.urls")),
+        re_path(
             r"^sitemap\.xml$",
             sitemap,
             {"sitemaps": {"cmspages": CMSSitemap}},
             name="django.contrib.sitemaps.views.sitemap",
         ),
         # custom urls
-        url(r"^login/", chameleon_os_login.custom_login, name="login"),
-        url(r"^logout/", chameleon_os_login.custom_logout, name="logout"),
-        url(r"^register/", chameleon_views.OIDCRegisterView.as_view(), name="register"),
+        re_path(r"^login/", chameleon_os_login.custom_login, name="login"),
+        re_path(r"^logout/", chameleon_os_login.custom_logout, name="logout"),
+        re_path(r"^register/", chameleon_views.OIDCRegisterView.as_view(), name="register"),
         # Rollout endpoints for new login
-        url(
+        re_path(
             r"^auth/force-password-login/$",
             chameleon_views.force_password_login,
             name="force_password_login",
         ),
-        url(
+        re_path(
             r"^auth/confirm/$",
             chameleon_os_login.confirm_legacy_credentials,
             name="federation_confirm_legacy_credentials",
         ),
-        url(
+        re_path(
             r"^user/migrate/$",
             chameleon_views.migrate,
             name="federation_migrate_account",
         ),
-        url(r"^api/user/migrate/status/$", chameleon_views.api_migration_state),
-        url(r"^api/user/migrate/job/$", chameleon_views.api_migration_job),
+        re_path(r"^api/user/migrate/status/$", chameleon_views.api_migration_state),
+        re_path(r"^api/user/migrate/job/$", chameleon_views.api_migration_job),
         # Legacy account endpoints
-        url(r"^user/", include("tas.urls", namespace="tas")),
-        url(r"^password-reset/$", chameleon_views.password_reset),
-        url(r"^user/dashboard/", chameleon_views.dashboard, name="dashboard"),
-        url(
+        re_path(r"^user/", include("tas.urls", namespace="tas")),
+        re_path(r"^password-reset/$", chameleon_views.password_reset),
+        re_path(r"^user/dashboard/", chameleon_views.dashboard, name="dashboard"),
+        re_path(
             r"^feed\.xml",
             RedirectView.as_view(permanent=True, url=reverse_lazy("user_news:feed")),
         ),
-        url(r"^user/outages/$", OutageListView.as_view(), name="outage_list"),
-        url(r"^user/outages/rss/$", OutageFeed(), name="outage_feed"),
-        url(
+        re_path(r"^user/outages/$", OutageListView.as_view(), name="outage_list"),
+        re_path(r"^user/outages/rss/$", OutageFeed(), name="outage_feed"),
+        re_path(
             r"^user/outages/(?P<slug>[-_\w]+)/$",
             OutageDetailView.as_view(),
             name="outage_detail",
         ),
-        url(r"^hardware/", include("g5k_discovery.urls", namespace="hardware")),
+        re_path(r"^hardware/", include("g5k_discovery.urls", namespace="hardware")),
         # Unclear if this legacy route still needs to be supported
-        url(
+        re_path(
             r"^user/discovery/",
             RedirectView.as_view(
                 permanent=True, url=reverse_lazy("hardware:discovery")
             ),
         ),
-        url(r"^user/projects/", include("projects.urls", namespace="projects")),
-        url(r"^user/help/", include("djangoRT.urls", namespace="djangoRT")),
-        url(
+        re_path(r"^user/projects/", include("projects.urls", namespace="projects")),
+        re_path(r"^user/help/", include("djangoRT.urls", namespace="djangoRT")),
+        re_path(
             r"^user/early-user-program/",
             include("cc_early_user_support.urls", namespace="cc_early_user_support"),
         ),
-        url(
+        re_path(
             r"^user/webinar/",
             include("webinar_registration.urls", namespace="webinar_registration"),
         ),
         # mailing list resource for mailman autosubscribe
-        url(
+        re_path(
             r"^mailman/new_members.txt$",
             chameleon_mailman_views.mailman_export_list,
             name="mailman_export_list",
         ),
         # ensure default djangocms_blog namespace is registered at /blog
         # (the auto-setup hook doesn't work well if the page is moved in the hierarchy)
-        url(r"^blog/", include("djangocms_blog.urls", namespace="Blog")),
+        re_path(r"^blog/", include("djangocms_blog.urls", namespace="Blog")),
         # robots.txt
-        url(
+        re_path(
             r"^robots.txt$",
             TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
         ),
         # cms urls
-        url(r"^taggit_autosuggest/", include("taggit_autosuggest.urls")),
-        url(r"^", include("blog_comments.urls")),
-        url(r"^", include("cms.urls")),
+        re_path(r"^taggit_autosuggest/", include("taggit_autosuggest.urls")),
+        re_path(r"^", include("blog_comments.urls")),
+        re_path(r"^", include("cms.urls")),
         # /appliances is bound to appliance_catalog app via CMS integration
         # /share is bound to sharing_portal app via CMS integration
         # /news is bound to user_news app via CMS integration
