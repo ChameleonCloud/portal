b990c3678e0f503fe495c5a292f67cbdc2d32ae9
Anish Reddy Ravula <2anishreddy@gmail.com>
2023-05-17 16:03:21 -0500

    Fix the source fields in Publication model and remove the constraint which is not working

--- projects/migrations/0020_duplicate_publications.py
+++ projects/migrations/0020_duplicate_publications.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0018_publication_sources_with_data_migrationshell'),
+        ('projects', '0019_publication_sources_with_data_migrationshell'),
     ]
 
     operations = [
