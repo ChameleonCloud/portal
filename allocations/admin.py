e08c6df7f3de7da3ca5a0fa1fe2bb0fed5034a26
Anish Reddy Ravula <2anishreddy@gmail.com>
2024-01-24 16:39:16 -0600

    Allocations admin view (read only)
    This does not replace the existing allocatins approval page
    
    Read only and cannot be used to change details
    
    This is done so project manager can look at allocations and use that data to get insights into them

--- allocations/admin.py
+++ allocations/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Charge
+from .models import Charge, Allocation
+
+
+class AllocationAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    list_display= (
+        'project',
+        'status',
+        'date_requested',
+        'date_reviewed',
+        'reviewer',
+    )
+    ordering = ["-date_requested"]
+
 
 admin.site.register(Charge)
+admin.site.register(Allocation, AllocationAdmin)
