460c67485cf0c5a539530388685e332729c70902
Mark Powers <markpowers@uchicago.edu>
2026-05-07 17:21:38 -0500

    Format

--- projects/user_publication/utils.py
+++ projects/user_publication/utils.py
@@ -50,11 +50,16 @@ def get_publications_with_same_attributes(pub, publication_model_class):
         similar_pub = publication_model_class.objects.filter(doi__iexact=pub.doi)
 
     # also find any publications with raw_publications with same source_id as the raw publication being imported
-    similar_pub = similar_pub | publication_model_class.objects.filter(
-        raw_publications__source_id__in=pub.raw_publications.values_list(
-            "source_id", flat=True
+    similar_pub = (
+        similar_pub
+        | publication_model_class.objects.filter(
+            raw_publications__source_id__in=pub.raw_publications.values_list(
+                "source_id", flat=True
+            )
         )
-    ).exclude(id=pub.id).distinct()
+        .exclude(id=pub.id)
+        .distinct()
+    )
 
     # Order results: approved first, then others. Return as a list so the
     # caller can rely on ordering.
