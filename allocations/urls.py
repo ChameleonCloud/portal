49c1bb347e86a03a365e0c7531e9f7117f362878
Mark Powers <markpowers@uchicago.edu>
2026-08-07 11:39:52 -0500

    Remove old allocations SPA

--- allocations/urls.py
+++ allocations/urls.py
@@ -1,22 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 app_name = "allocations"
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("api/view/<str:charge_code>/", views.view_project, name="view_project"),
-    path("view/", views.view, name="view"),
-    path("json/", views.return_json, name="return_json"),
     path("approval/", views.approval, name="approval"),
     path("contact/", views.contact, name="contact"),
-    path("denied/", views.denied, name="denied"),
-    path("user/", views.user_select, name="user_select"),
-    path("user/<str:username>/", views.user_projects, name="user_projects"),
-    re_path(
-        r"^template/(?P<resource>.+?)\.html/$",
-        views.allocations_template,
-        name="allocations_template",
-    ),
 ]
